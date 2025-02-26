7b4c0dc4c5d187cd3840dfa4dbf6c6c22d6ab346
Collin Brennan <collinbrennan8@gmail.com>
2025-02-26 09:34:42 -0500

    add endpoint to fetch product items

--- server/actions/product.actions.ts
+++ server/actions/product.actions.ts
@@ -1,11 +1,32 @@
-import type { Product } from '@prisma/client'
+import type { Item, Product } from '@prisma/client'
 import prisma from '../lib/prisma.ts'
+import type { OptionFieldsData } from '../schema/item.schema.ts'
 import type { ProductData } from '../schema/product.schema.ts'
 
 export async function getProducts(): Promise<Product[]> {
   const products = prisma.product.findMany()
 
   return products
+}
+
+export async function getProductWithItemsBySlug(slug: string) {
+  const productWithItems = await prisma.product.findFirst({
+    where: { slug },
+    include: {
+      items: true,
+    },
+  })
+
+  // prisma doesn't know the shape of option field json
+  type ProductWithItems = Product & {
+    items: Array<
+      Omit<Item, 'optionFields'> & {
+        optionFields: OptionFieldsData | null
+      }
+    >
+  }
+
+  return productWithItems as ProductWithItems
 }
 
 export async function createProduct(
--- server/routes/product.route.ts
+++ server/routes/product.route.ts
@@ -1,10 +1,20 @@
-import { createProductSchema } from '../schema/product.schema.ts'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
-import { createProduct, getProducts } from '../actions/product.actions.ts'
+import {
+  createProduct,
+  getProducts,
+  getProductWithItemsBySlug,
+} from '../actions/product.actions.ts'
 import { adminProcedure, procedure, router } from '../lib/trpc.ts'
 import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+import { createProductSchema } from '../schema/product.schema.ts'
 
 const productRouter = router({
+  get: procedure.input(z.string()).query(async ({ input }) => {
+    const product = await getProductWithItemsBySlug(input)
+    return product
+  }),
+
   list: procedure.query(async () => {
     const products = await getProducts()
     return products
